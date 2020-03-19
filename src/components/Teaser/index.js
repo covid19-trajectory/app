62500d1bafa0a4f9751b7f6e266839cb45eb97fc
Krzysztof Olbinski <krzysztof.olbinski@toptal.com>
2020-03-19 01:16:56 +0100

    18 marca

--- src/components/Teaser/index.js
+++ src/components/Teaser/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
-export default ({ id, title, date }) => (
-  <Link className="Teaser" to={id}>
-    <h2>{title}</h2>
-    {date && <div className="Teaser__date">{date}</div>}
-  </Link>
-);
+export default ({ id, title, date }) => {
+  const onClick = useCallback(() => window.scrollTo(0, 0), []);
+  return (
+    <Link className="Teaser" to={id} onClick={onClick}>
+      <h2>{title}</h2>
+      {date && <div className="Teaser__date">{date}</div>}
+    </Link>
+  );
+};
